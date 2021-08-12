a6fa4398de2f53b71701ea680d674a28c3e7aabf
Marcos Ramos <marcos.ramos@boavista.local>
2021-08-12 15:51:21 -0300

    commit comentário driver

--- setup/Driver.java
+++ setup/Driver.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 public class Driver {
     public WebDriver WebDriver() {
         //Informar referência do seu driver...
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\marcos.ramos\\drivers\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\marcos.ramos\\drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://mercadolivre.com.br");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
